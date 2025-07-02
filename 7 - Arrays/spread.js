5efad30325e02ddd498c9a1eaba0fbd81c5eda6e
Marcus Lima <boymarcu1@gmail.com>
2025-07-01 23:29:12 -0300

    literalmente 1 colchete

--- 7 - Arrays/spread.js
+++ 7 - Arrays/spread.js
@@ -5,7 +5,7 @@
 const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]
 ];
 
-console.log(numeros[1][2]]); // acessando o índice 6
+console.log(numeros[1][2]); // acessando o índice 6
 
 // nesta forma nós utilizamos a vírgula para pular o índice 0
 // dentro do índice um colocamos mais um par de [] e utilizamos mais vírgula para pular os demais índices
