c8a5301ed855c45958782905ccd04be553599cb0
Marcus Lima <boymarcu1@gmail.com>
2025-07-07 20:21:39 -0300

    finalização da aula

--- 8 - Estrutura de Repetição/for_classic.js
+++ 8 - Estrutura de Repetição/for_classic.js
@@ -23,3 +23,11 @@ for (let i = 0; i <= 10; i++) {
     const par = i % 2 === 0 ? 'par' : 'impar';
     console.log(i, par);
 }
+
+
+// Percorrendo um Array
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`, frutas[i]);
+}
