6b6f47ca986e9a1a510fd58e79f4e5204885de09
Marcus Lima <boymarcu1@gmail.com>
2025-07-07 20:15:53 -0300

    atualização da aula

--- 8 - Estrutura de Repetição/for_classic.js
+++ 8 - Estrutura de Repetição/for_classic.js
@@ -6,5 +6,20 @@
 // console.log('Linha 5');
 
 for (let i = 0; i <= 5 ; i++) {
-    console.log(`Linha ${i}}`);
+    console.log(`Linha ${i}`);
+}
+
+// Caso precise que essa estrutura de repetição pule por um valor X específico:
+
+for (let i = 0; i <=20; i += 2) {
+    console.log(`Linha ${i}`);
+}
+
+// Também pode começar do negativo, ou inverter
+
+// Usando uma estrutura de repetição para conferir se o valor é PAR ou ÍMPAR:
+
+for (let i = 0; i <= 10; i++) {
+    const par = i % 2 === 0 ? 'par' : 'impar';
+    console.log(i, par);
 }
